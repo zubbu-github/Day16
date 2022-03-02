d4e71a965623dfcd88ce30220f0c7ab10db9c656
LENOVO <mdzubeir2468@gmail.com>
2022-03-02 18:35:19 +0530

    day 18 Retrieve Program

--- Day16/src/day16/JDBCDemo.java
+++ Day16/src/day16/JDBCDemo.java
@@ -11,13 +11,23 @@ public class JDBCDemo {
 		
 		Statement st = con.createStatement();
 		//String query = "create table student(ID int primary key,SName varchar(20),SMarks int)";
-		String query1 = "insert into student values (3,'Tariq',30)";
+		String query1 = "insert into student values (4,'Tariq',30)";
+		//String query2 ="delete from student  where ID =  1";
+		//String query3 ="update student Set SName = 'Kalpesh' where ID = 3";
+		ResultSet rs = st.executeQuery("Select ID, SName, SMarks from student");
+		while(rs.next()) {
+			System.out.println(rs.getString(1));
+			System.out.println(rs.getString(2));
+			System.out.println(rs.getString(3));
+		}
 		
 		//st.executeUpdate(query);
 		st.executeUpdate(query1);
+		//st.executeUpdate(query2);
 		
-		//System.out.println("Student Table Created Successfully");
-		System.out.println("Row inserted Successfully");
+		System.out.println("Student Table Created Successfully");
+		//System.out.println("Row inserted Successfully");
+		//System.out.println("Row deleted Successfully");
 		
 		st.close();
 		con.close();
